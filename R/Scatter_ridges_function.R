2d5f27af4d68ad5f55b629e8912fd190fd474ca9
Matthieu Bourgery <matthieu.bourgery@gmail.com>
2023-11-14 11:25:50 +0200

    R/Scatter_ridges_function.R

--- R/Scatter_ridges_function.R
+++ R/Scatter_ridges_function.R
@@ -1,32 +1,40 @@
 #' Scatter plot combine with ridge lines
 #'
-#' @param x as input data. If a dataframe was provided, the dataframe should contain no less than three columns.  If no dataframe was supplied, a x vector should be set as an input. The vector should contain numerical values.
-#' @param y as input data if . If no dataframe was supplied, a y vector should be set as an input along with a x vector. The vector should contain numerical values.
-#' @param xlab to give a title for the xlab can be given here.
-#' @param ylab to give a title for the ylab can be given here.
-#' @param title to give a title for the plot can be given here.
-#' @param xlim to set scale limits on the xaxis. 
-#' @param ylim to set scale limits on the yaxis.
-#' @param group
-#' @param color The user can choose from ggpubr::get_palette. Default = "lancet".
+#' @description `ggScatRidges` is a simple function combining a scatter plot generated in 'ggplot2' to a ridgeline plot from 'ggridges' to visualise the disparities of the data points. This helps visualising the distribution of different groups in the data.
+#'
+#' @param x As input data. If a dataframe was provided, the dataframe should contain no less than three columns. If no dataframe was supplied, a x vector should be set as an input. The vector should #' contain numerical values.
+#' @param y As input data. If no dataframe was provided, a y vector should be set as an input along with a x vector. The vector should contain numerical values.
+#' @param xlab To give a title for the xlab can be given here.
+#' @param ylab To give a title for the ylab can be given here.
+#' @param title To give a title for the plot can be given here.
+#' @param xlim To set scale limits on the xaxis.
+#' @param ylim To set scale limits on the yaxis.
+#' @param group The user should provide here the grouping of the rows if a dataframe was provided, otherwise a vector.
+#' @param color The user can choose from `ggpubr::get_palette`. Default = "lancet".
 #' @param ridges The user can choose to plot, or not, the ridgelines. Default = TRUE.
 #' @param size The overall size of the text in the plot. Default = 15.
-#' @param draw if the user wants to directly draw the plot. Default = TRUE.
+#' @param draw If the user wants to directly draw the plot. Default = TRUE.
+#'
+#' @return A ggplot object.
+#' 
+#' @examples
+#' \dontrun{
+#' # The following example is based on the iris dataset:
+#'
+#' ggScatRidges(x = iris$Sepal.Length, y = iris$Sepal.Width, group= iris$Species, 
+#'                  color = "lancet", ridges = T, title = "plot iris",
+#'                  xlab = "Sepal.Length", ylab = "Sepal.Width", size = 15, draw = T) 
+#'}
+#'
 #' @import ggplot2
-#' @import cowplot
 #' @import ggpubr
+#' @importFrom cowplot axis_canvas insert_xaxis_grob insert_yaxis_grob ggdraw
 #' @import ggridges
 #' @import viridis
 #' @import hrbrthemes
+#'
 #' @export 
 
-
-library("ggplot2")
-library("cowplot")
-library("ggpubr")
-library("ggridges")
-library("viridis")
-library("hrbrthemes")
 
 # Main plot
 ggScatRidges <- function(x,
@@ -92,7 +100,7 @@ ggScatRidges <- function(x,
       geom_density2d()
     
     xridges <- suppressMessages({
-      axis_canvas(main_plot, axis = "x") +
+      cowplot::axis_canvas(main_plot, axis = "x") +
       geom_density_ridges(mapping = aes(x = x, y = group, fill = group),
                           alpha = 0.7, size = 0.2) +
       ggpubr::fill_palette(color) +
@@ -100,7 +108,7 @@ ggScatRidges <- function(x,
       })
     
     yridges <- suppressMessages({
-      axis_canvas(main_plot, axis = "y", coord_flip = TRUE)+
+      cowplot::axis_canvas(main_plot, axis = "y", coord_flip = TRUE)+
       geom_density_ridges(mapping = aes(x = y, y = group, fill = group),
                           alpha = 0.7, size = 0.2) +
       scale_y_discrete(expand = c(0, 0)) +
@@ -124,16 +132,12 @@ ggScatRidges <- function(x,
   }
   
   if(draw){
-    ggdraw(final)
+    cowplot::ggdraw(final)
   }else{
     return(invisible(final))
   }
 }
 
-
-ggScatRidges(x = iris$Sepal.Length, y = iris$Sepal.Width, group= iris$Species, 
-                    color = "lancet", ridges = T, title = "plot iris",
-                    xlab = "xlab", ylab = "ylab", size = 25, draw = T) 
 
 
 

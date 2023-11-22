8a7d87c9a534283cdbcd4d1029aa7c48ad709b00
Matthieu Bourgery <matthieu.bourgery@gmail.com>
2023-11-22 18:10:12 +0200

    Add several parameters to the function

--- R/Scatter_ridges_function.R
+++ R/Scatter_ridges_function.R
@@ -12,9 +12,12 @@
 #' @param group The user should provide here the grouping of the rows if a dataframe was provided, otherwise a vector.
 #' @param color The user can choose from `ggpubr::get_palette`. Default = "lancet".
 #' @param ridges The user can choose to plot, or not, the ridgelines. Default = TRUE.
-#' @param size The overall size of the text in the plot. Default = 15.
+#' @param base_size The overall size of the text in the plot. Default = 15.
+#' @param size The size of the dots in the plot. Default = 3
+#' @param pch The user can change the shape of the points by providing a vector length equal to the number of groups.
 #' @param draw If the user wants to directly draw the plot. Default = TRUE.
 #' @param density_2d If the user wants to add density contours around group of points on the plot. Default = TRUE.
+#' @param legend If the user wants to add or remove the legend. Default = TRUE.
 #' @param label If the user wants to add custom labels for each point. Default = FALSE
 #' @param text The user can give a vector to add labels or directly provide it as a fourth column from a dataframe.
 #'
@@ -58,13 +61,16 @@ ggScatRidges <- function(x,
                          group = NULL,
                          color = "lancet",
                          ridges = TRUE,
-                         size = 15,
+                         base_size = 15,
+                         size = 2,
+                         pch = NULL,
                          draw = TRUE,
                          density_2d = TRUE,
+                         legend = TRUE,
                          label = FALSE,
                          text = NULL){
-
-# Check user input --------------------------------------------------------
+  
+  # Check user input --------------------------------------------------------
   if(!inherits(x, "data.frame")){
     # then your x is vector and you need to get y
     if(is.null(y)){
@@ -81,22 +87,22 @@ ggScatRidges <- function(x,
     }
   }else if(label){
     if(ncol(x) != 4L){
-    stop("if 'x' is a tabular data, it should have four columns, first will be used as 'x' axis and the second column will be used as the 'y' axis. The third column will be used for grouping and the fourth for text labels.")
+      stop("if 'x' is a tabular data, it should have four columns, first will be used as 'x' axis and the second column will be used as the 'y' axis. The third column will be used for grouping and the fourth for text labels.")
     }else{
       y <-     x[[2]]
       group <- x[[3]]
       text <-  x[[4]]
       x <-     x[[1]]
-      }
+    }
   }else {
     if(ncol(x) != 3L){
       stop("if 'x' is a tabular data, it should have three columns, first will be used as 'x' axis and the second column will be used as the 'y' axis. The third column will be used for grouping.")
     }else{
-        y <-     x[[2]]
-        group <- x[[3]]
-        x <-     x[[1]]
-      }
+      y <-     x[[2]]
+      group <- x[[3]]
+      x <-     x[[1]]
     }
+  }
   if(!inherits(x, c("double", "numeric"))){
     stop("The 'x' column to be plotted should only contain numeric values")
   }
@@ -111,32 +117,36 @@ ggScatRidges <- function(x,
   }
   if(label)
     if(missing(text) & length(text) != length(x)){
-      stop("A vector must be provided to label the data points. The vector should have the same size as the 'x' vector")
+      stop("A vector must be provided to label the data points. The vector should have the same length as the 'x' vector")
     }
-    
+  
   # Scatter plot ------------------------------------------------------------
   if(ridges){
     main_plot <- ggplot(mapping = aes(x = x, y = y, col = group)) +
-      geom_point() +
+      geom_point(aes(shape = group), size = size) +
       xlab(xlab) +
       ylab(ylab) +
-      theme_minimal(base_size = size) +
+      scale_shape_manual(values=pch)+
+      theme_minimal(base_size = base_size) +
       theme(panel.border = element_rect(colour = "black", fill = NA, linewidth = 1)) +
       ggpubr::color_palette(color) +
       scale_x_continuous(limits = xlim) +
       scale_y_continuous(limits = ylim) +
       ggtitle(title)
-      
+    
     if(density_2d){
       main_plot <- main_plot + ggplot2::geom_density2d()}
     
     if(label){
       main_plot <- main_plot + ggrepel::geom_text_repel(
-        # data = subset(resdata, resdata$padj < 0.05 & abs(resdata$log2FoldChange) > 1.5),
         aes(label = text),
         max.overlaps = Inf,
-        size=8
+        size=6
       )
+    }
+    
+    if(legend == FALSE){
+      main_plot <- main_plot + theme(legend.position="none")
     }
     
     # Add x axis ridges -------------------------------------------------------
@@ -165,10 +175,11 @@ ggScatRidges <- function(x,
     # Scatter plot only if Ridges = F -----------------------------------------
   }else{
     final <- ggplot(mapping = aes(x = x, y = y, col = group)) +
-      geom_point() +
+      geom_point(aes(shape = group), size = 2) +
       xlab(xlab) +
       ylab(ylab) +
-      theme_minimal(base_size = size) +
+      scale_shape_manual(values=pch)+
+      theme_minimal(base_size = base_size) +
       theme(panel.border = element_rect(colour = "black", fill = NA, linewidth = 1)) +
       ggpubr::color_palette(color) +
       scale_x_continuous(limits = xlim) +
@@ -180,13 +191,16 @@ ggScatRidges <- function(x,
     
     if(label){
       final <- final + ggrepel::geom_text_repel(
-        # data = subset(resdata, resdata$padj < 0.05 & abs(resdata$log2FoldChange) > 1.5),
         aes(label = text),
         max.overlaps = Inf,
-        size=8
+        size=6
       )
     }
+    
+    if(legend == FALSE){
+      final <- final + theme(legend.position="none")
     }
+  }
   
   # Return objects ----------------------------------------------------------
   if(draw){
